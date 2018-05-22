4fac1408e0637e2b98370687bab0e616b55c5846
braquino <32620833+braquino@users.noreply.github.com>
2018-05-21 22:56:37 -0300

    Add files via upload

--- preprocessing.R
+++ preprocessing.R
@@ -56,3 +56,46 @@ create_formula <- function(data){
 }
 
 
+bairros_func <- function(){
+  x1 = -74.008619
+  y1 = 40.85939
+  x2 = -73.91533
+  y2 = 40.70466
+  n_col = 2
+  n_row = 3
+  largura = x1 - x2
+  altura = y1 - y2
+  lar_quad = largura / n_col
+  alt_quad = altura / n_row
+  bairros = c()
+  
+  for (row in seq(y1, y2 + alt_quad, by=-alt_quad)){
+    for (col in seq(x1, x2 + lar_quad, by=-lar_quad)){
+      bairros = c(bairros, c(col, row, col - lar_quad, row - alt_quad))
+    }
+  }
+  matrix(bairros, ncol=4, byrow=T)
+}
+
+bairros = bairros_func()
+aeroporto1 = c( -73.848374, 40.688240, -73.723494, 40.615962)
+aeroporto2 = c(-73.894189, 40.786195, -73.844867, 40.75191)
+aeroportos = matrix(c(aeroporto1, aeroporto2), ncol=4, byrow = T)
+bairros = rbind(bairros, aeroportos)
+
+define_bairro <- function(x, y){
+  result = nrow(bairros) + 1
+  for (i in seq(1:nrow(bairros))) {
+    x1 = bairros[i,1]
+    y1 = bairros[i,2]
+    x2 = bairros[i,3]
+    y2 = bairros[i,4]
+    if (x > x1 && x < x2 && y < y1 && y > y2){
+      result = i
+      break()
+    }
+  }
+  result
+}
+
+define_bairro(-73.9736, 40.7611)
